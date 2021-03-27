9df2108b1f8eff4295494dfa42733dcd251021bd
rmatin20 <31660978+rmatin20@users.noreply.github.com>
2021-03-27 12:10:52 -0500

    Add files via upload

---
+++ TypeText.java
@@ -0,0 +1,40 @@
+package sendKeys;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TypeText {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rmati\\eclipse-workspace\\Feb202021\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+				
+		driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("email")).sendKeys("matt@gmail.com");
+		System.out.println("email typed");
+		
+	//	//Press Enter: Use Selenium Enum Keys with its constant ENTER.
+	//	driver.findElement(By.id("input")).sendKeys(Keys.ENTER); 
+	//	
+		//Press TAB key in Selenium: Press any keyboard key using Keys class.
+	//	driver.findElement(By.id("input")).sendKeys(Keys.TAB);
+		
+		//enter a text in all capital letter:
+		driver.findElement(By.xpath("//input[@type='search']")).sendKeys(Keys.SHIFT, "hello"); //prints HELLO
+		
+		driver.close();
+	}
+}
+
+//noSuchElementException
+
